29fbaee536a699f8f9a972121cf30c971df30073
Pablo Carbonell <pablocarbonell@icloud.com>
2021-07-26 19:08:45 +0300

    fix: lazyloader

--- src/components/BadgeGroup/BadgeGroup.tsx
+++ src/components/BadgeGroup/BadgeGroup.tsx
@@ -1,4 +1,6 @@
 import './BadgeGroup.css';
+import { forceCheck } from 'react-lazyload';
+import { useEffect } from 'react';
 import { IBadgeGroup } from '../../shared/types';
 import Badge from '../Badge/Badge';
 
@@ -7,6 +9,10 @@ interface Props {
 }
 
 export default function BadgeGroup({ badgeGroup }: Props): JSX.Element {
+  useEffect(() => {
+    setTimeout(forceCheck, 50);
+  }, []);
+
   return (
     <div className="BadgeGroup">
       <h1>{badgeGroup.title}</h1>
