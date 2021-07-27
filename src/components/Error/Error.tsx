650f215169ac829bfa645bb28782deac2b0cf16c
Pablo Carbonell <pablocarbonell@icloud.com>
2021-07-27 18:33:26 +0300

    fix: error message

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function Error({ error }: Props): JSX.Element {
   return (
     <div className="Error">
-      <h2>No badges</h2>
+      <h2>{error}</h2>
     </div>
   );
 }
