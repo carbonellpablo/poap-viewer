2e2479eb483aa0054ebfb340b5555b8ec62e3c84
Pablo Carbonell <pablocarbonell@icloud.com>
2021-07-27 17:51:43 +0300

    feat: Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,19 @@
 import './Loading.css';
+import { useEffect } from 'react';
+import loadingGif from './loading.gif';
 
 export default function Loading(): JSX.Element {
-  // setTimeout and if 5 seconds pass, reload the page
+  // if it is loading for more than 5 seconds, refresh the whole app.
+  useEffect(() => {
+    // eslint-disable-next-line
+    const timmer = setTimeout(() => location.reload(), 5000);
 
-  return <div className="Loading">loading</div>;
+    return () => clearTimeout(timmer);
+  }, []);
+
+  return (
+    <div className="Loading">
+      <img alt="loading" src={loadingGif} />
+    </div>
+  );
 }
